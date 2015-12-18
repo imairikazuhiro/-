a116caeb3423af76544cb5659d2615ac69521754
imairikazuhiro <imairikazuhiro@gmail.com>
2015-12-18 18:59:34 +0900

    ddd
    ddddddd

---
+++ Assets/NewBehaviourScript.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NewBehaviourScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		int a, b, c, d;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
